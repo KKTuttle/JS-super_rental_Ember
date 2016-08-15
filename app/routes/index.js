a64d0c0fa733277fa6625b63a4e327ab0c4110d0
Katarina Tuttle and Rony Nasr <katarina+rony@>
2016-08-15 14:53:52 -0700

    allow user to display, create and delete announcements

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,25 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return Ember.RSVP.hash({
-      rental: this.store.findAll('rental'),
-      announcement: this.store.findAll('announcement')
-    });
+    return this.store.findAll('rental');
   },
   actions: {
     save3(params) {
-     var newRental = this.store.createRecord('rental', params);
-     newRental.save();
-     this.transitionTo('index');
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
     },
     destroyRental(rental) {
-     rental.destroyRecord();
-     this.transitionTo('index');
-   },
-   save3(params) {
-     var newAnnouncement = this.store.createRecord('announcement', params);
-     newAnnouncement.save();
-     this.transitionTo('index')
-   },
- }
+      rental.destroyRecord();
+      this.transitionTo('index');
+    },
+
+  }
 });
